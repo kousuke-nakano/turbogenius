66bc0180a8402916cfb52985fc9fa294e2b61c9f
kousuke-nakano <kousuke_1123@icloud.com>
2022-08-24 11:15:41 +0200

    bug fixed in makefort10_genius.py again, for the basis part, do not use element_symbols_supercell, but use structure.element_symbols

--- turbogenius/makefort10_genius.py
+++ turbogenius/makefort10_genius.py
@@ -310,12 +310,13 @@ class Makefort10_genius(GeniusIO):
                     return data_set_found, data_choice
 
         # note! element_symbols depends on the number of supercell size!!!
+        # note! for the basis part, do not use element_symbols_supercell, but use structure.element_symbols
         element_symbols_supercell = structure.element_symbols * self.supercell[0] * self.supercell[1] * self.supercell[2]
 
         # det. basis set!
         if isinstance(det_basis_set, str):
             det_basis_files = []; det_basis_choice = {}
-            for element in element_symbols_supercell:
+            for element in structure.element_symbols:
                 if pseudo_potential is None: # all-electron
                     det_basis_sets_list=glob.glob(os.path.join(turbo_genius_tmp_dir, "basis_set", "BSE", f"{element}_{det_basis_set}*.basis"))
                     det_basis_chosen, det_basis_choice = database_founder(data_sets_list=det_basis_sets_list, element=element, data_choice=det_basis_choice, prefix="basis_set")
@@ -335,7 +336,7 @@ class Makefort10_genius(GeniusIO):
         # jas. basis set
         if isinstance(jas_basis_set, str):
             jas_basis_files = []; jas_basis_choice = {}
-            for element in element_symbols_supercell:
+            for element in structure.element_symbols:
                 if pseudo_potential is None: # all-electron
                     jas_basis_sets_list=glob.glob(os.path.join(turbo_genius_tmp_dir, "basis_set", "BSE", f"{element}_{jas_basis_set}*.basis"))
                     jas_basis_chosen, jas_basis_choice = database_founder(data_sets_list=jas_basis_sets_list, element=element, data_choice=jas_basis_choice, prefix="basis_set")
@@ -397,13 +398,13 @@ class Makefort10_genius(GeniusIO):
         if det_cut_basis_option:
             logger.info("cutbasis for Det. part.")
             # cut basis, det_basis, according to Andrea Zen's criteria, exponents > 8 * Z^2
-            for nuc, element in enumerate(element_symbols_supercell):
+            for nuc, element in enumerate(structure.element_symbols):
                 thr_exp= 8 * return_atomic_number(element) ** 2
                 det_basis_sets.cut_orbitals(thr_exp=thr_exp, nucleus_index=nuc, method="larger")
         if jas_cut_basis_option:
             logger.info("cutbasis for Jas. part.")
             # cut basis, jas_basis, according to max criteria, exponents > max (det part)
-            for nuc, element in enumerate(element_symbols_supercell):
+            for nuc, element in enumerate(structure.element_symbols):
                 # to be refactored!! Is this appropriate for the Jastrow part??
                 thr_exp= 4 * return_atomic_number(element) # not 8*Z**2 but 4*Z
                 jas_basis_sets.cut_orbitals(thr_exp=thr_exp, nucleus_index=nuc, method="larger")
